751dd08e74d8eed7f8f7bb9d8ca6a6c858b2349d
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-04 13:56:39 -0300

    adicionando o products service ao provider do modulo da rotine de produtos

--- src/app/feature-module/products/products.module.ts
+++ src/app/feature-module/products/products.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ProductsComponent} from './products.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import {ProductsService} from '../service/products/products.service';
 
 @NgModule({
   declarations: [ProductsComponent, ProductsListComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [ProductsService]
 })
 export class ProductsModule { }
