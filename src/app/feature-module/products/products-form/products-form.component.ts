9bbe42552483ac3b966fc68f981ef43043f96fa1
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-04 19:08:30 -0300

    Criando metodos para edicao de produto: populando formulario e salvar

--- src/app/feature-module/products/products-form/products-form.component.ts
+++ src/app/feature-module/products/products-form/products-form.component.ts
@@ -1,13 +1,16 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Product} from '../../../shared/domain/product/product';
 
 @Component({
   selector: 'app-products-form',
   templateUrl: './products-form.component.html',
   styleUrls: ['./products-form.component.scss']
 })
-export class ProductsFormComponent implements OnInit {
+export class ProductsFormComponent implements OnInit, OnChanges {
   @Output() productEventEmitter = new EventEmitter();
+  @Output() productEditEventEmitter = new EventEmitter();
+  @Input() product: Product;
   productForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
@@ -17,8 +20,17 @@ export class ProductsFormComponent implements OnInit {
     this.loadProductForm();
   }
 
-  loadProductForm() {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.product) {
+      if (this.product) {
+        this.prepareEdit();
+      }
+    }
+  }
+
+  private loadProductForm() {
     this.productForm = this.formBuilder.group({
+      id: [null],
       code: [null, [Validators.required]],
       name: [null, [Validators.required]],
       registerDate: [null, [Validators.required]],
@@ -26,8 +38,33 @@ export class ProductsFormComponent implements OnInit {
     });
   }
 
-  submit() {
+  private submit() {
     const product = this.productForm.getRawValue();
     this.productEventEmitter.emit(product);
+    this.productForm.reset();
+  }
+
+  private prepareEdit() {
+    this.productForm.patchValue({
+      id: this.product.id,
+      code: this.product.code,
+      name: this.product.name,
+      registerDate: this.product.registerDate,
+      value: this.product.value
+    });
+  }
+
+  private edit() {
+    const product = this.productForm.getRawValue();
+    this.productEditEventEmitter.emit(product);
+    this.productForm.reset();
+  }
+
+  public handleSubmit() {
+    if (!this.product) {
+      this.submit();
+    } else {
+      this.edit();
+    }
   }
 }
