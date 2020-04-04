b6928a05e8804bc29edeb90c6c8ae0fb59b87def
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-04 14:01:33 -0300

    Criando variaveis input e output para controle de estado dos valores da listagem

--- src/app/feature-module/products/products-list/products-list.component.ts
+++ src/app/feature-module/products/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Product} from '../../../shared/domain/product/product';
+import {Cols} from '../../../shared/domain/cols/cols';
 
 @Component({
   selector: 'app-products-list',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  @Input() products: Product[] = [];
+  @Output() productEventEmitter = new EventEmitter();
+  cols: Cols[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.loadCols();
+  }
+
+  loadCols() {
+    this.cols = [
+      {field: '', header: 'id', type: 'text', headerStyle: '', contentStyle: ''},
+      {field: '', header: 'Código', type: 'text', headerStyle: '', contentStyle: ''},
+      {field: '', header: 'Nome', type: 'text', headerStyle: '', contentStyle: ''},
+      {field: '', header: 'Registro', type: 'date', headerStyle: '', contentStyle: ''},
+      {field: '', header: 'Valor', type: 'currency', headerStyle: '', contentStyle: ''},
+    ];
   }
 
 }
