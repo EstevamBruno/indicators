c94069050e57efb6a11a39cda1e8d41b18defede
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-04 16:23:08 -0300

    Importando reactformsmodule

--- src/app/feature-module/products/products.module.ts
+++ src/app/feature-module/products/products.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import {ProductsComponent} from './products.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import {ProductsService} from '../service/products/products.service';
+import { ProductsFormComponent } from './products-form/products-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [ProductsComponent, ProductsListComponent],
+  declarations: [ProductsComponent, ProductsListComponent, ProductsFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ],
   providers: [ProductsService]
 })
