4f06c9efb6d2cbf7d1d6b5adf470714ec215b7ac
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 18:22:36 -0300

    Importando moduloes e declarando componentes importantes para o funcionamente da rotina

--- src/app/feature-module/indicators/indicators.module.ts
+++ src/app/feature-module/indicators/indicators.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {IndicatorsComponent} from './indicators.component';
+import { ProductIndicatorListComponent } from './product-indicator-list/product-indicator-list.component';
+import { SaleIndicatorListComponent } from './sale-indicator-list/sale-indicator-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RegisterDateTableComponent } from './product-indicator-list/register-date-table/register-date-table.component';
+import { ProductRegisterTableComponent } from './product-indicator-list/product-register-table/product-register-table.component';
 
 @NgModule({
-  declarations: [IndicatorsComponent],
+  declarations: [IndicatorsComponent, ProductIndicatorListComponent, SaleIndicatorListComponent, RegisterDateTableComponent, ProductRegisterTableComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class IndicatorsModule { }
