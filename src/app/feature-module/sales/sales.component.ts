9b27538673d89c191ff931bd33ce4d36337e11ba
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 13:08:05 -0300

    Efetuando cadastro e listagem de vendas

--- src/app/feature-module/sales/sales.component.ts
+++ src/app/feature-module/sales/sales.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Sale} from '../../shared/domain/sales/sale';
+import {SalesService} from '../service/sales/sales.service';
 
 @Component({
   selector: 'app-sales',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent implements OnInit {
+  sales: Sale[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private salesService: SalesService) {
   }
 
+  ngOnInit() {
+    this.getSales();
+  }
+
+  private getSales() {
+    this.salesService.getSales().subscribe(sales => {
+      this.sales = sales;
+    }, error => console.log(error));
+  }
+
+  submit(event) {
+    this.salesService.createSale(event).subscribe(() => {
+      alert('venda criada com sucesso');
+      this.getSales();
+    }, error => console.log(error));
+  }
 }
