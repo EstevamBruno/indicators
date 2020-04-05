4e16817df5dcb73b46bd1c314f1d314b35b4c7d5
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 13:06:22 -0300

    Criando listagem de vendas

--- src/app/feature-module/sales/sales-list/sales-list.component.ts
+++ src/app/feature-module/sales/sales-list/sales-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Sale} from '../../../shared/domain/sales/sale';
+import {Cols} from '../../../shared/domain/cols/cols';
 
 @Component({
   selector: 'app-sales-list',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-list.component.scss']
 })
 export class SalesListComponent implements OnInit {
+  @Input() sales: Sale[] = [];
+  cols: Cols[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.loadCols();
+  }
+
+  private loadCols() {
+    this.cols = [
+      {field: '', header: 'id', type: 'text', headerStyle: '', contentStyle: ''},
+      {field: '', header: 'Código do Produto', type: 'text', headerStyle: '', contentStyle: ''},
+      {field: '', header: 'Data', type: 'date', headerStyle: '', contentStyle: ''},
+    ];
   }
 
 }
