94e1fecf72219bdc6cb3e4c4265a76006aa87947
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 14:58:15 -0300

    Criando metodos de get para vendas e produtos

--- src/app/feature-module/sales-indicators/sales-indicators.component.ts
+++ src/app/feature-module/sales-indicators/sales-indicators.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../shared/domain/product/product';
+import {Sale} from '../../shared/domain/sales/sale';
+import {SalesService} from '../service/sales/sales.service';
+import {ProductsService} from '../service/products/products.service';
 
 @Component({
   selector: 'app-sales-indicators',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-indicators.component.scss']
 })
 export class SalesIndicatorsComponent implements OnInit {
+  sales: Sale[] = [];
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private salesService: SalesService,
+              private productsService: ProductsService) {
+  }
 
   ngOnInit() {
+    this.getSales();
+    this.getProducts();
+  }
+
+  private getSales() {
+    this.salesService.getSales().subscribe(sales => {
+      console.log(sales);
+      this.sales = sales;
+    }, error => console.log(error));
+  }
+
+  private getProducts() {
+    this.productsService.getProducts().subscribe(products => {
+      console.log(products);
+      this.products = products;
+    }, error => console.log(error));
   }
 
 }
