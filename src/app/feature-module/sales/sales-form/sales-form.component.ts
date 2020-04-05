5957f3f25c12d42f91baf68ec45e7192c7a4b575
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 13:02:17 -0300

    Criando formulario para cadastro de venda

--- src/app/feature-module/sales/sales-form/sales-form.component.ts
+++ src/app/feature-module/sales/sales-form/sales-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sales-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-form.component.scss']
 })
 export class SalesFormComponent implements OnInit {
+  @Output() saleEventEmitter = new EventEmitter();
+  saleForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loadSaleForm();
   }
 
+  private loadSaleForm() {
+    this.saleForm = this.formBuilder.group({
+      id: [null],
+      productCode: [null, [Validators.required]],
+      date: [null, [Validators.required]]
+    });
+  }
+
+  private submit() {
+    const sale = this.saleForm.getRawValue();
+    this.saleEventEmitter.emit(sale);
+    this.saleForm.reset();
+  }
 }
