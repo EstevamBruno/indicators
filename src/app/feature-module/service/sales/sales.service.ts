7b674de56ff9832b04804c4a9fcf405fdeb2fe21
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 12:58:23 -0300

    Criando metodos de get e create de vendas

--- src/app/feature-module/service/sales/sales.service.ts
+++ src/app/feature-module/service/sales/sales.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from '../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {Sale} from '../../../shared/domain/sales/sale';
+import {Observable} from 'rxjs';
 
 const VENDAS = `${environment.JSON_SERVER}/vendas`;
 
 @Injectable()
 export class SalesService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+
+  createSale(sale: Sale): Observable<Sale> {
+    return this.http.post<Sale>(VENDAS, sale);
+  }
+
+  getSales(): Observable<Sale[]> {
+    return this.http.get<Sale[]>(VENDAS);
+  }
 }
