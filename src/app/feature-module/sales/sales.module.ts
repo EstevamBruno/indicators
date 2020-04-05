4d7cd3d6a87f17432f6c6d67cea751da8af238da
Bruno Kennedy <brunokennedy16@gmail.com>
2020-04-05 12:14:20 -0300

    Declarando novos componentes no module de vendas: list e form

--- src/app/feature-module/sales/sales.module.ts
+++ src/app/feature-module/sales/sales.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SalesComponent} from './sales.component';
 import {SalesService} from '../service/sales/sales.service';
+import { SalesListComponent } from './sales-list/sales-list.component';
+import { SalesFormComponent } from './sales-form/sales-form.component';
 
 @NgModule({
-  declarations: [SalesComponent],
+  declarations: [SalesComponent, SalesListComponent, SalesFormComponent],
   imports: [
     CommonModule
   ],
